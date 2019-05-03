8ea5bbe2e2bc91d1a740a965daf467c7cee92033
Connor Onweller <onweller@udel.edu>
2019-05-03 19:56:40 -0400

    ugh

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,5 @@
 public class Main {
-    class Main {
-        public static void main(String args[]) {
-            System.out.println("Hello, world");
-        }
+    public static void main(String args[]) {
+        System.out.println("Hello, world");
     }
 }
