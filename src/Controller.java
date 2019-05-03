1f8ecb02770afbc18f8cb047b415506a34f4821d
Chris Outhwaite <chrisout@udel.edu>
2019-05-03 19:46:47 -0400

    OK

---
+++ src/Controller.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+
+public class Controller {
+	
+	public Controller(JFrame j) {
+		
+		View view = new View(); 
+		Model model = new Model();
+		
+		
+	}
+
+}
---
+++ src/Model.java
@@ -0,0 +1,8 @@
+
+public class Model {
+	
+	public Model() {
+		
+	}
+
+}
