0148a7c5899d79890fda9308e67767b4e35656e5
Chris Outhwaite <chrisout@udel.edu>
2019-05-03 21:32:02 -0400

    Just Controller Stuff

--- src/Controller.java
+++ src/Controller.java
@@ -1,3 +1,6 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
 import javax.swing.JFrame;
 
 public class Controller {
@@ -7,6 +10,45 @@ public class Controller {
 		View view = new View(); 
 		Model model = new Model();
 		
+		
+		view.addKeyInput(new KeyInput); 
+		
+		
+		
+	}
+	
+	class KeyInput implements KeyListener{
+		
+		String input; 
+
+		@Override
+		public void keyPressed(KeyEvent keyEvent) {
+			// TODO Auto-generated method stub
+			int code = keyEvent.getKeyCode();
+			
+			switch (code) {
+				case 1: 
+					 
+				case 2:
+					
+				case 3:
+					
+				case 4: 		
+			}
+			
+		}
+
+		@Override
+		public void keyReleased(KeyEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void keyTyped(KeyEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
 		
 	}
 
