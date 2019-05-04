46038ac6004eed91f973983279ab0ee33b7b4cf4
mariavanv <49887053+mariavanv@users.noreply.github.com>
2019-05-03 21:32:28 -0400

    Update Model.java

--- src/Model.java
+++ src/Model.java
@@ -2,7 +2,40 @@
 public class Model {
 	
 	public Model() {
+		int score = 0;
+		int money = 50;
+		int environmentStatus = 3;
+		
+		int fish = 3;
+		int erosion = 5;
+		int trash = 0;
+		
+		int moneyDelta = 10;
+		int trashDelta =  1;
+		int fishDelta = 0;
+		int erosionDelta = 0;
 		
 	}
-
+	
+	public void update ()
+	{
+		money = money + moneyDelta;
+		trash = trash + trashDelta;
+		fish = fish + fishDelta;
+		erosion = erosion + erosionDelta;
+		
+	}
+	
+	public void removeTrash
+	{
+		trash--;
+		money = money - 2;
+	}
+	
+	public void hireGroundsKeeper 
+	{
+		trashDelta = trashDelta - 1;
+		money = money - 20;
+	}
+	
 }
